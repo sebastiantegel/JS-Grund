529bf6ede6b34aec73ab6da163f7196f1fcc60eb
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2021-11-17 14:51:02 +0100

    Objects and typescript

--- Lektion_10_20211116/js/main.js
+++ Lektion_10_20211116/js/main.js
@@ -1,5 +1,6 @@
 window.onload = function () {
-    document.getElementById("save").addEventListener("click", handleClick);
+    var _a;
+    (_a = document.getElementById("save")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", handleClick);
     var firstname = "Sebastian";
     var x = 17;
     var i = 10;
@@ -7,6 +8,7 @@ window.onload = function () {
     console.log(firstname);
     var sum = addition(10, 17);
     console.log(sum);
+    getMovies();
 };
 function addition(x, y) {
     return x + y;
@@ -18,4 +20,24 @@ function handleClick() {
     var span = document.createElement("span");
     span.innerHTML = userText;
     document.body.appendChild(span);
+}
+var Movie = /** @class */ (function () {
+    function Movie(imageUrl, title, year, imdb) {
+        this.image = imageUrl;
+        this.title = title;
+        this.released = year;
+        this.imdbID = imdb;
+    }
+    return Movie;
+}());
+function getMovies() {
+    fetch("http://www.omdbapi.com/?s=star&apikey=416ed51a")
+        .then(function (response) { return response.json(); })
+        .then(function (result) {
+        console.log(result.Search);
+        var movies = result.Search.map(function (movie) {
+            return new Movie(movie.Poster, movie.Title, movie.Year, movie.imdbID);
+        });
+        console.log(movies);
+    });
 }
--- Lektion_10_20211116/ts/main.ts
+++ Lektion_10_20211116/ts/main.ts
@@ -1,5 +1,5 @@
 window.onload = function () {
-  document.getElementById("save").addEventListener("click", handleClick);
+  document.getElementById("save")?.addEventListener("click", handleClick);
 
   let firstname: string = "Sebastian";
 
@@ -12,6 +12,8 @@ window.onload = function () {
 
   let sum = addition(10, 17);
   console.log(sum);
+
+  getMovies();
 };
 
 function addition(x: number, y: number): number {
@@ -29,4 +31,43 @@ function handleClick() {
   let span: HTMLSpanElement = document.createElement("span");
   span.innerHTML = userText;
   document.body.appendChild(span);
+}
+
+interface IMovie {
+  Poster: string;
+  Title: string;
+  Year: number;
+  imdbID: string;
+}
+
+interface IOmdbResponse {
+  Search: IMovie[];
+}
+
+class Movie {
+  image: string;
+  title: string;
+  released: number;
+  imdbID: string;
+
+  constructor(imageUrl: string, title: string, year: number, imdb: string) {
+    this.image = imageUrl;
+    this.title = title;
+    this.released = year;
+    this.imdbID = imdb;
+  }
+}
+
+function getMovies() {
+  fetch("http://www.omdbapi.com/?s=star&apikey=416ed51a")
+    .then((response: Response) => response.json())
+    .then((result: IOmdbResponse) => {
+      console.log(result.Search);
+
+      let movies: Movie[] = result.Search.map((movie: IMovie) => {
+        return new Movie(movie.Poster, movie.Title, movie.Year, movie.imdbID);
+      });
+
+      console.log(movies);
+    });
 }
